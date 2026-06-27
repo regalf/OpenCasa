83f7760d1774fcd0abdb9ff224401a11003b84fa
regalf <regal@m35155552@gmail.com>
2026-06-27 03:37:55 +0200

    mc-server: store data in HOME/DATA/mc-server/ per wiki spec
    - revert OPENCASA_DATA_DIR approach
    - use HOME/DATA/mc-server/ (wiki App Data Storage spec)

--- examples/apps/mc-server/app.py
+++ examples/apps/mc-server/app.py
@@ -5,8 +5,8 @@ import http.server, json, os, subprocess, socket, mimetypes, threading, time, ur
 HOST = '127.0.0.1'
 PORT = int(os.environ.get('OPENCASA_APP_PORT', '18995'))
 DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.environ.get('OPENCASA_DATA_DIR', '/usr/local/webui')
-SERVER_DIR = os.path.join(DATA_DIR, 'mc-server')
+HOME = os.environ.get('HOME', '/home/opencasa')
+SERVER_DIR = os.path.join(HOME, 'DATA', 'mc-server')
 
 def _find_binary():
     for name in ('bareiron', 'bareiron.exe', 'bareiron-macppc'):
